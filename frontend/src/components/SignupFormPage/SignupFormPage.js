09a672409b53d5c26a532977d5f3441f892aaae7
Pierre Guilbault <guilbaultpj@gmail.com>
2021-07-17 18:35:08 +0200

    sign up page

--- frontend/src/components/SignupFormPage/SignupFormPage.js
+++ frontend/src/components/SignupFormPage/SignupFormPage.js
@@ -1,18 +1,19 @@
-import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Redirect } from "react-router-dom";
-import * as sessionActions from "../../store/session";
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import * as sessionActions from '../../store/session';
+import './SignupForm.css';
 
 function SignupFormPage() {
   const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
-  const [email, setEmail] = useState("");
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   const [errors, setErrors] = useState([]);
 
-  if (sessionUser) return <Redirect to="/" />;
+  if (sessionUser) return <Redirect to='/' />;
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -28,48 +29,74 @@ function SignupFormPage() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <ul>
-        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-      </ul>
-      <label>
-        Email
-        <input
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Username
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Password
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Confirm Password
-        <input
-          type="password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          required
-        />
-      </label>
-      <button type="submit">Sign Up</button>
-    </form>
+    <div className='signup-container'>
+      <form className='signup-form' onSubmit={handleSubmit}>
+        <ul>
+          {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+        </ul>
+        <div className='signup-form__input-container'>
+        <i className='fas fa-at'>
+          <input
+            className='signup-form__input'
+            placeholder='Email Address'
+            type='text'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </i>
+        </div>
+        <div className='signup-form__input-container'>
+          <i className='fas fa-user'>
+            <input
+              className='signup-form__input'
+              placeholder='Username'
+              type='text'
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </i>
+        </div>
+        <div className='signup-form__input-container'>
+          <i className='fas fa-lock'>
+            <input
+              className='signup-form__input'
+              placeholder='Password'
+              type='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              />
+          </i>
+        </div>
+        <div className='signup-form__input-container'>
+          <i className='fas fa-lock'>
+            <input
+              className='signup-form__input'
+              placeholder='Confirm your Password'
+              type='password'
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+            />
+          </i>
+        </div>
+        <div className='signup-form__checkbox-container'>
+          <p className='signup-form__text'>18 or older?</p>
+          <i className='fas fa-beer'>
+            <input
+              // TODO implement background check
+              className='signup-form__checkbox'
+              placeholder='Confirm your Password'
+              type='checkbox'
+              required
+            />
+          </i>
+        </div>
+        <button className='signup-button' type='submit'>Start Sipping</button>
+      </form>
+    </div>
   );
 }
 
