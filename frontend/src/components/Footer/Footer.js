ba287171cb764f4d9f520643b9f2acd8d1ad0aa2
Pierre Guilbault <guilbaultpj@gmail.com>
2021-09-26 08:16:54 -0400

    add angellist and profile and make off site links

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -5,13 +5,16 @@ export default function Footer() {
         <footer className='footer'>
             <p className='footer__designed-by'>Designed By:</p>
             <div className='footer__name-link'>
-                <h3 className='footer__name'>Pierre Guilbault</h3>
+                <h3 className='footer__name'><a className='footer__link' target="_blank" rel="noreferrer" href='https://theguilbotine.github.io/Portfolio/'>Pierre Guilbault</a></h3>
                 <div className='footer__icons'>
-                    <a className='footer__link' href='https://github.com/TheGuilbotine/GoodCIPA-app'>
+                    <a className='footer__link' target="_blank" rel="noreferrer" href='https://github.com/TheGuilbotine/GoodCIPA-app'>
                         <i class="fab fa-github-square"></i>
                     </a>
-                    <a className='footer__link' href='https://www.linkedin.com/in/pierre-guilbault-30020549/'>
+                    <a className='footer__link' target="_blank" rel="noreferrer" href='https://www.linkedin.com/in/pierre-guilbault-30020549/'>
                         <i class="fab fa-linkedin"></i>
+                    </a>
+                    <a className='footer__link' target="_blank" rel="noreferrer" href='https://angel.co/u/pierre-guilbault-1'>
+                        <i class="fab fa-angellist"></i>
                     </a>
                 </div>
             </div>
